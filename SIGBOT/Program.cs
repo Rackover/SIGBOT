560561878d608ce9ee27f7a269732136228b77a8
rackover <rackover@racknet.noip.me>
2019-10-01 19:59:21 +0200

    Initial commit

---
+++ SIGBOT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SIGBOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
